629a8a2fc7d88c81acbeb52ac315d91843f5d676
James Rogers <jarogers095@gmail.com>
2020-01-26 09:37:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,3 +5,10 @@ def square_array(array)
   end
   return squared_array
 end
+
+def square_array_using_collect(array)
+  squared_array = array.collect { 
+    |element| element * element 
+  }
+  return squared_array
+end
